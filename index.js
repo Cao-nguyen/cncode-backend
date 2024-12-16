8bc131687b850c34c283ec5d71620be09a651f3c
Cao-nguyen <chinhphucnguvan@gmail.com>
2024-12-16 20:51:12 +0700

    v

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 require('dotenv').config()
+const db = require('./config/mongoDB')
+
 const app = express()
 const port = process.env.PORT
+
+// Kết nối database (mongoDB)
+db.connect()
 
 app.get('/', (req, res) => {
     res.send('Hello Cao Nguyên')

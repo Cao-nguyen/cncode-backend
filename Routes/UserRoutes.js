b8b07293cce8297af016caf35a85a64ba663f88e
Cao-nguyen <chinhphucnguvan@gmail.com>
2024-12-26 19:36:17 +0700

    update 26.12

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const UserControllers = require('../controllers/UserControllers');
+// const express = require('express');
+// const UserControllers = require('../controllers/UserControllers');
 
-const router = express.Router();
+// const router = express.Router();
 
-const userRoutes = (app) => {
-    router.post('/api/v1/dangky', UserControllers.registerUser);
-    router.post('/api/v1/xacthuc', UserControllers.xacthuc);
-    router.post('/api/v1/quenmatkhau', UserControllers.quenmatkhau);
-    router.post('/api/v1/dangnhap', UserControllers.login);
+// // const userRoutes = (app) => {
+// //     router.post('/api/v1/dangky', UserControllers.registerUser);
+// //     router.post('/api/v1/xacthuc', UserControllers.xacthuc);
+// //     router.post('/api/v1/quenmatkhau', UserControllers.quenmatkhau);
+// //     router.post('/api/v1/dangnhap', UserControllers.login);
 
-    app.use(router);
-};
+// //     app.use(router);
+// // };
 
-module.exports = userRoutes;
+// module.exports = userRoutes;

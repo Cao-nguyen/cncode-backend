33d5c25d051ecb4b96f1af736585c6ddfaafc6d5
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-02-09 19:27:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ app.use(
     origin: [
       "https://cncode.vercel.app", // Frontend URL
       "http://localhost:3000", // Localhost khi phát triển
-      "https://cncode-git-main-cao-nguyens-projects-a928ddee.vercel.app/"
     ],
   })
 );

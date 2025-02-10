2ca4b63840245933cd6988caedce6280e936cb7e
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-02-10 08:37:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ db.connect();
 app.use(
   cors({
     origin: [
-      "https://cncode.vercel.app", // Frontend URL
-      "http://localhost:3000", // Localhost khi phát triển
+      "*"
     ],
   })
 );

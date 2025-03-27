fdf42b0ef8d64ab04e7a86c2963b10dac903c384
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-03-27 13:26:15 +0700

    push 27.3.2025

--- config/mongoDB.js
+++ config/mongoDB.js
@@ -1,13 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 module.exports.connect = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        console.log('Kết nối thành công tới MongoDB')
-    } catch (error) {
-        console.error('Kết nối thất bại:', error.message)
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {});
+    console.log("Kết nối thành công tới MongoDB");
+  } catch (error) {
+    console.error("Kết nối thất bại:", error.message);
+  }
 };

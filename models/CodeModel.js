654dc0253cfcb72d93b13fc1804fe137abc94e02
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-04-23 19:38:56 +0700

    Update CodeModel.js

--- models/CodeModel.js
+++ models/CodeModel.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const CodeSchema = new mongoose.Schema({
   email: { type: String },
   code: { type: String },
-  expiresAt: { type: Date }, // Thêm thời gian hết hạn
+  expiresAt: {
+    type: Date,
+    default: Date.now(),
+    expires: 600,
+  },
 });
 
 module.exports = mongoose.model("Code", CodeSchema);

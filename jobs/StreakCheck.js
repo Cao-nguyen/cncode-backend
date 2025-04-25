047af5fc1fd7c168763f18467fc93f69c99cd3fe
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-04-26 06:56:24 +0700

    Update StreakCheck.js

--- jobs/StreakCheck.js
+++ jobs/StreakCheck.js
@@ -3,27 +3,31 @@ const User = require("../models/UserModel");
 const Coin = require("../models/CoinsModel");
 
 const startResetStreakJob = () => {
-  cron.schedule("50 6 * * *", async () => {
-    console.log("🔍 Đang kiểm tra người dùng không có coin...");
+  cron.schedule(
+    "0 7 * * *",
+    async () => {
+      console.log("🔍 Đang kiểm tra người dùng không có coin...");
 
-    try {
-      const allUsers = await User.find();
-      for (const user of allUsers) {
-        const hasCoin = await Coin.findOne({ authorId: user._id });
-        if (!hasCoin) {
-          await User.findByIdAndUpdate(user._id, { streak: 0 });
-          console.log(`🔄 Đã reset streak cho user: ${user._id}`);
+      try {
+        const allUsers = await User.find();
+        for (const user of allUsers) {
+          const hasCoin = await Coin.findOne({ authorId: user._id });
+          if (!hasCoin) {
+            await User.findByIdAndUpdate(user._id, { streak: 0 });
+            console.log(`🔄 Đã reset streak cho user: ${user._id}`);
+          }
         }
+
+        await Coin.deleteMany({});
+        console.log("🗑️ Đã xóa toàn bộ dữ liệu trong Coin.");
+
+        console.log("✅ Đã hoàn tất kiểm tra.");
+      } catch (err) {
+        console.error("❌ Lỗi khi chạy cron job:", err);
       }
-
-      await Coin.deleteMany({});
-      console.log("🗑️ Đã xóa toàn bộ dữ liệu trong Coin.");
-
-      console.log("✅ Đã hoàn tất kiểm tra.");
-    } catch (err) {
-      console.error("❌ Lỗi khi chạy cron job:", err);
-    }
-  });
+    },
+    { timezone: "Asia/Ho_Chi_Minh" }
+  );
 };
 
 module.exports = startResetStreakJob;
